7675e9648acda672b98a30dfbe5623b93df34848
kamesh2711 <kamesh.n@stratos.studio>
2022-02-01 21:32:05 +0530

    first commit

---
+++ force-app/main/default/lwc/webToLead/webToLead.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+// import backgroundUrl from '@salesforce/resourceUrl/webToLead';
+
+export default class WebToLead extends LightningElement {
+
+//      get backgroundStyle() {
+//   return `height:50rem;background-image:url(${backgroundUrl})`;
+       
+//      }
+    
+}
